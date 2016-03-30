29239a44df7d83e8462d5a17c9114f506f46310c
zliu <7275337@qq.com>
2016-03-31 00:41:08 +0800

    lof discount done

--- qetf.py
+++ qetf.py
@@ -8,6 +8,10 @@ try:
 except ImportError:
     from urllib2 import urlopen, Request
 
+import tushare as ts
+	
+con_redeem = -0.9
+#con_redeem = 10
 
 myreq = Request('http://www.jisilu.cn/data/lof/stock_lof_list/?___t=1459332174309')
 myreq.add_header("Accept-Language", "en-US,en;q=0.5")
@@ -21,10 +25,22 @@ i=0
 for i in range(len(lof_list)):
 	mydicts=lof_list[i]
 	subdict=mydicts['cell']
-	print(subdict['fund_id'], subdict['fund_nm'], subdict['price'],  subdict['increase_rt'], subdict['fund_nav'], subdict['estimate_value'], subdict['discount_rt'], subdict['stock_ratio']) #subdict['apply_fee'], subdict['redeem_fee']
+	discount_rt = float(subdict['discount_rt'][:-1])
+	
+
+	
+	if(discount_rt < con_redeem):
+		df = ts.get_realtime_quotes(subdict['fund_id'])
+		this = df.loc[0]
+		a1_p = float(this.a1_p)
+		nav = float(subdict['fund_nav'])
+	
+		real_rt= -(nav-a1_p)*100/a1_p
+		real_rt=round(real_rt,3)
+		print(subdict['fund_id'], subdict['fund_nm'], subdict['price'], '*',this.a1_p,real_rt,'%', subdict['fund_nav'], subdict['estimate_value'], subdict['discount_rt']) #subdict['apply_fee'], subdict['redeem_fee']
 		
 print("once more")
-myreq = Request('http://www.jisilu.cn/data/lof/stock_lof_list/?___t=1459332382795')
+myreq = Request('http://www.jisilu.cn/data/lof/index_lof_list/?___t=1459354520266')
 myreq.add_header("Accept-Language", "en-US,en;q=0.5")
 myreq.add_header("Connection", "keep-alive")	
 myreq.add_header('Referer', "http://www.jisilu.cn/")
@@ -36,7 +52,22 @@ i=0
 for i in range(len(lof_list)):
 	mydicts=lof_list[i]
 	subdict=mydicts['cell']
-	print(subdict['fund_id'], subdict['fund_nm'], subdict['price'],  subdict['increase_rt'], subdict['fund_nav'], subdict['estimate_value'], subdict['discount_rt'], subdict['stock_ratio']) #subdict['apply_fee'], subdict['redeem_fee']
+	discount_rt = float(subdict['discount_rt'][:-1])
+	
+	if(discount_rt < con_redeem):
+		df = ts.get_realtime_quotes(subdict['fund_id'])
+		this = df.loc[0]
+		a1_p = float(this.a1_p)
+		nav = float(subdict['fund_nav'])
+		real_rt= -(nav-a1_p)*100/a1_p
+		real_rt=round(real_rt,3)
+		print(subdict['fund_id'], subdict['fund_nm'], subdict['price'], '*',this.a1_p,real_rt,'%', subdict['fund_nav'], subdict['estimate_value'], subdict['discount_rt']) #subdict['apply_fee'], subdict['redeem_fee']
+		
+
+ 
+ 
+ 
+ #subdict['apply_fee'], subdict['redeem_fee']
 				
 		
 		
@@ -47,4 +78,4 @@ for i in range(len(lof_list)):
 #POST /data/lof/stock_lof_list/?___t=1459332382795 HTTP/1.1
 
 
-
+#POST /data/lof/index_lof_list/?___t=1459354520266 HTTP/1.1
