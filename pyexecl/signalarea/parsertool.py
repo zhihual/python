dc0bf4e1809d16aef99277494eee91e98a4e490e
zhihual <coolzh_ts@hotmail.com>
2017-07-06 11:18:25 +0800

    add offline meter analysis report

--- pyexecl/signalarea/parsertool.py
+++ pyexecl/signalarea/parsertool.py
@@ -25,21 +25,25 @@ sys.setdefaultencoding('utf-8')
 def openWhiteList(excelName):  #excelname 电表名单excel表格,返回名单handle
     print(excelName)
     wb = load_workbook(excelName)
-    #ws = wb.active
-    #print(len(tuple(ws.rows)), len(tuple(ws.columns)))
+    ws = wb.active
+    row_num = len(tuple(ws.rows))
+    print('row_num', row_num)
+    for i in range (1, row_num):
+        ws['C'+str(i)].value = 0
     return wb
 
 
-def queryMeterAddr(excelhdl, meterId):#根据电表id,在电子白名单中查询地址
+def queryMeterAddr(excelhdl, meterId):#根据电表id,在电子白名单中查询地址, 并且将C行值设1
     row_num = len(tuple(excelhdl.rows))
     columns_num = len(tuple(excelhdl.columns))
     #print(row_num, columns_num)
-    for idx in range(1,row_num):
+    for idx in range(1,row_num+1):
         #print(excelhdl['A'+str(idx)].value, excelhdl['B'+str(idx)].value)
         val = str(excelhdl['A'+str(idx)].value)
         val = val.strip()
         if val == meterId:
             val = str(excelhdl['B'+str(idx)].value)
+            excelhdl['C'+str(idx)].value = 1 
             return val
     return ''
 
@@ -61,6 +65,23 @@ def convertMacToMeterId(mac):#将mac地址转换为表id, return meterId
 #def convertMeterIdToMac(meterId):#Convert meterId to Mac style. return Mac   
 
 
+def queryOfflineMeter(excelhdl,offline_list):
+    online_num = 0
+    offline_num = 0
+    row_num = len(tuple(excelhdl.rows))
+    for i in range(1, row_num+1):
+        local = excelhdl['C'+str(i)].value
+        if local == 1:
+            online_num +=1
+        else:
+            offline_num+=1
+            temp_str = str(excelhdl['A'+str(i)].value)
+            temp_str += '    '
+            temp_str +=str(excelhdl['B'+str(i)].value)
+            offline_list.append(temp_str)
+    #print('Last', i)
+    return (online_num, offline_num, row_num)
+
 def openTopoTxt(topoTxtName):#topo txt file name, return handle
     print(topoTxtName)
     f = fopen(topoTxtName)
@@ -70,10 +91,27 @@ def openTopoTxt(topoTxtName):#topo txt file name, return handle
 def putInfobyMac(topohdl, infoStr, placeMark):#output infoStr to topo table
     print('hello')
 
+
+
+
+
+#Test MeterId
+
+def Test_QueryMeterAddr():  
+    tMeterId = '110120061848'
+    tMeterAddr = queryMeterAddr(whiteListSheet, tMeterId)
+    print(tMeterId, tMeterAddr.decode('gbk'))
+
+#Test Convert Meter Mac
+
+def Test_MacToMeterId():
+    tMeterMac = '18:77:06:20:01:11'
+    tConverMeterId = convertMacToMeterId(tMeterMac)
+    print(tMeterMac)
+    print(tConverMeterId)
 #begin of main function
 
 #main()
-
 whitelListName='whitelist.xlsx'
 
 whiteList = Workbook() 
@@ -84,45 +122,41 @@ whiteList= openWhiteList(whitelListName)
 
 whiteListSheet = whiteList.active
 
-
-
-
-#Test Code Code
-#Test open excel
 print(len(tuple(whiteListSheet.rows)), len(tuple(whiteListSheet.columns)))
 
+topo_list_content = []
 
-
-#Test MeterId
-tMeterId = '110120061848'
-
-tMeterAddr = queryMeterAddr(whiteListSheet, tMeterId)
-print(tMeterId, tMeterAddr.decode('gbk'))
-
-#Test Convert Meter Mac
-tMeterMac = '18:77:06:20:01:11'
-
-tConverMeterId = convertMacToMeterId(tMeterMac)
-print(tMeterMac)
-print(tConverMeterId)
-
-#Test open txt
-_list_content = []
+offline_list = []
 
 fh=open(topoTxtName, 'rb')
 
-for i in fh.readlines():
-    _list_content.append(i)
+for line in fh.readlines():
+    topo_list_content.append(line)
 fh.close()
 
-_content = ''
-for i in _list_content:
-    if(i.find(':') >= 0):
-        meter_id = convertMacToMeterId(i)
+result_content = ''
+for line in topo_list_content:
+    if(line.find(':') >= 0):
+        meter_id = convertMacToMeterId(line)
         addr = queryMeterAddr(whiteListSheet, meter_id)
-        _content += i[:-3] +'    '+ addr +'\r\n'
+        result_content += line[:-3] +'    '+ addr +'\r\n'
 
-open('result.txt', 'wb').writelines(_content)        
+(online, offline, total) = queryOfflineMeter(whiteListSheet, offline_list)
+
+print('Online:', online,'Offline:', offline, 'Total:', total)
+
+offline_result_content = ''
+offline_result_content += ('Online:'+str(online)+'  Offline:'+str(offline)+'  Total:'+str(total)+'\r\n')
+result_content+=('Online:'+str(online)+'  Offline:'+str(offline)+'  Total:'+str(total)+'\r\n')
+
+for line in offline_list:
+    result_content+=line+'\r\n'
+    offline_result_content+=line+'\r\n'
+        
+open('result.txt', 'wb').writelines(result_content)
+open('offline_result.txt', 'wb').writelines(offline_result_content)
+
+
 
 
 
